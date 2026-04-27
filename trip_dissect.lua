01569d1a9245b4b2f6ad87c5bd58aec27edcefe5
arf20 <aruizfernandez05@gmail.com>
2026-04-27 21:26:30 +0200

    NOTIFICATION decode

--- trip_dissect.lua
+++ trip_dissect.lua
@@ -28,6 +28,9 @@ trip_attr_seq = ProtoField.int32("trip.msg.update.attr.lsencap.seq", "Sequence n
 trip_route_len = ProtoField.uint16("trip.msg.update.attr.route.len", "Length", base.DEC)
 trip_route_prefix = ProtoField.string("trip.msg.update.attr.route.pfx", "Prefix")
 
+trip_notif_code = ProtoField.uint8("trip.msg.notif.code", "Code", base.DEC)
+trip_notif_subcode = ProtoField.uint8("trip.msg.notif.subcode", "Subcode", base.DEC)
+
 trip_proto.fields = {
 	trip_msg_len,
 	trip_msg_type,
@@ -50,6 +53,8 @@ trip_proto.fields = {
 	trip_attr_seq,
 	trip_route_len,
 	trip_route_prefix,
+	trip_notif_code,
+	trip_notif_subcode,
 }
 
 -- create a function to dissect it
@@ -73,11 +78,16 @@ function trip_proto.dissector(buffer, pinfo, tree)
 	if msg_type_num == 1 then
 		local open_subtree = subtree:add(trip_proto, buffer(), "OPEN")
 
+		local itad = buffer(7, 4):le_uint()
+		local id = buffer(11, 4):le_uint()
+
 		open_subtree:add_le(trip_open_ver, buffer(3, 1))
 		open_subtree:add_le(trip_open_hold, buffer(5, 2))
 		open_subtree:add_le(trip_open_itad, buffer(7, 4))
-		open_subtree:add_le(trip_open_id, buffer(11, 4))
+		open_subtree:add_le(trip_open_id, buffer(11, 4)):append_text(" (" .. ipv4_to_str(id) .. ")")
 		open_subtree:add_le(trip_open_optslen, buffer(15, 2))
+
+		local info_detail = itad .. ":" .. ipv4_to_str(id)
 
 		local optslen = buffer(15, 2):le_uint()
 		local optoff = 17
@@ -136,13 +146,14 @@ function trip_proto.dissector(buffer, pinfo, tree)
 			optoff = optoff + (4 + optlen)
 		end
 
-		pinfo.cols.info = "OPEN"
+		pinfo.cols.info = "OPEN " .. info_detail
 	elseif msg_type_num == 2 then
 		local update_subtree = subtree:add(trip_proto, buffer(), "UPDATE")
 
+		local info_detail = nil
+
 		local attr_off = 3
 		while msg_len > 0 do
-			print("old msg_len " .. msg_len)
 			local attr_subtree = update_subtree:add(trip_proto, buffer(), "Attribute")
 
 			local attr_flags = buffer(attr_off, 1):le_uint()
@@ -164,27 +175,38 @@ function trip_proto.dissector(buffer, pinfo, tree)
 
 			if attr_type == 1 then
 				local routes_subtree = attr_subtree:add(trip_proto, buffer(), "WithdrawnRoutes")
-				dissect_routes(routes_subtree, buffer, attr_len, attr_val_off)
+				local rcount = dissect_routes(routes_subtree, buffer, attr_len, attr_val_off)
+				info_detail = "WithdrawnRoutes[" .. rcount .. "]"
 			elseif attr_type == 2 then
 				local routes_subtree = attr_subtree:add(trip_proto, buffer(), "ReachableRoutes")
-				dissect_routes(routes_subtree, buffer, attr_len, attr_val_off)
+				local rcount = dissect_routes(routes_subtree, buffer, attr_len, attr_val_off)
+				info_detail = "ReachableRoutes[" .. rcount .. "]"
 			end
 
 			msg_len = msg_len - ((attr_val_off + attr_len) - attr_off)
 			attr_off = attr_val_off + attr_len
-			print("attr_len " .. attr_len)
-			print("new msg_len " .. msg_len)
 		end
 
-		pinfo.cols.info = "UPDATE"
+		pinfo.cols.info = "UPDATE " .. info_detail
 	elseif msg_type_num == 3 then
-		pinfo.cols.info = "NOTIFICATION"
+		local notif_subtree = subtree:add(trip_proto, buffer(), "NOTIFICATION")
+
+		local code = buffer(3, 1):le_uint()
+		local subcode = buffer(4, 1):le_uint()
+
+		notif_subtree:add_le(trip_notif_code, buffer(3, 1)):append_text(" (" .. get_code_name(code) .. ")")
+		notif_subtree
+			:add_le(trip_notif_subcode, buffer(4, 1))
+			:append_text(" (" .. get_subcode_name(code, subcode) .. ")")
+
+		pinfo.cols.info = "NOTIFICATION " .. get_code_name(code) .. "," .. get_subcode_name(code, subcode)
 	elseif msg_type_num == 4 then
 		pinfo.cols.info = "KEEPALIVE"
 	end
 end
 
 function dissect_routes(tree, buffer, attr_len, route_off)
+	local count = 0
 	while attr_len > 0 do
 		local route_subtree = tree:add(trip_proto, buffer(), "Route")
 
@@ -200,7 +222,13 @@ function dissect_routes(tree, buffer, attr_len, route_off)
 
 		attr_len = attr_len - ((route_off + 6 + prefix_len) - route_off)
 		route_off = route_off + 6 + prefix_len
+		count = count + 1
 	end
+	return count
+end
+
+function ipv4_to_str(ip)
+	return string.format("%d.%d.%d.%d", (ip >> 24) & 0xff, (ip >> 16) & 0xff, (ip >> 8) & 0xff, ip & 0xff)
 end
 
 function get_str_len(buffer, off)
@@ -327,6 +355,72 @@ function get_attr_name(attr)
 		name = "ITAD Topology"
 	elseif attr == 11 then
 		name = "ConvertedRoute"
+	end
+
+	return name
+end
+
+function get_code_name(code)
+	local name = "Unknown"
+
+	if code == 1 then
+		name = "Message"
+	elseif code == 2 then
+		name = "Open"
+	elseif code == 3 then
+		name = "Update"
+	elseif code == 4 then
+		name = "Expired"
+	elseif code == 5 then
+		name = "State"
+	elseif code == 6 then
+		name = "Cease"
+	end
+
+	return name
+end
+
+function get_subcode_name(code, subcode)
+	local name = "Unknown"
+
+	if code == 1 then
+		if subcode == 1 then
+			name = "Bad length"
+		elseif subcode == 2 then
+			name = "Bad type"
+		end
+	elseif code == 2 then
+		if subcode == 1 then
+			name = "Unsupported version"
+		elseif subcode == 2 then
+			name = "Bad ITAD"
+		elseif subcode == 3 then
+			name = "Bad ID"
+		elseif subcode == 4 then
+			name = "Unsupported option"
+		elseif subcode == 5 then
+			name = "Bad hold time"
+		elseif subcode == 6 then
+			name = "Unsupported capability"
+		elseif subcode == 7 then
+			name = "Transmission mode mismatch"
+		end
+	elseif code == 3 then
+		if subcode == 1 then
+			name = "Malformed attribute"
+		elseif subcode == 2 then
+			name = "Unknown well-known attribute"
+		elseif subcode == 3 then
+			name = "Missing well-known flag"
+		elseif subcode == 4 then
+			name = "Bad attribute flag"
+		elseif subcode == 5 then
+			name = "Bad attribute length"
+		elseif subcode == 6 then
+			name = "Invalid attribute"
+		end
+	else
+		name = "Unused"
 	end
 
 	return name
